7fdfd2f07e05baf14f357e95643886c5058bcac6
Andriy730 <43599074+Andriy730@users.noreply.github.com>
2018-11-07 19:42:42 +0200

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ARR_SIZE 50
+int main()
+{
+    int n = 10;
+    int array[ARR_SIZE];
+    int array_new[ARR_SIZE];
+    int array_N[ARR_SIZE];
+    int a;
+    int j = 0;
+    
+    srand(time(0));
+    for(int i = 0; i < n; i++)
+    {
+        array[i] = rand()%10 +1;
+        printf("%i  ", array[i]);
+    }
+    printf("\n");
+    
+    printf("Enter number [1, 10]:");
+    scanf("%i", &a);
+    
+    for(int i = 0, k = 0; i < n; i++)
+    {
+        if(array[i] == a)
+        {
+            j++;
+        }    
+        if(array[i] != a)
+        {
+            array_new[k] = array[i];
+            k++;
+        }
+    }
+    
+    int c = 3*(n-j)/2;
+    
+    for(int i = 0, k = 0; i < c; i++, k++)
+    {
+        if(k%2 == 1)
+        {
+            array_N[i] = 0;
+            i++;
+        }
+        array_N[i] = array_new[k];
+    }
+    
+    for(int i = 0; i < c; i++)
+    {
+        printf("%i  ", array_N[i]);
+    }
+    printf("\n");
+}
